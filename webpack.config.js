1f39d97e06e9fd09f33b7983fc02357752c465ad
Dan Tao <daniel.tao@gmail.com>
2018-09-18 06:26:43 -0500

    Set up bare-bones webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'static/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
