e00964c4388f0a501736ce98fe8cec2b267c1bc3
Dan Tao <daniel.tao@gmail.com>
2018-09-23 19:51:23 -0400

    Show display name rather than username on index.html

--- django_starter/models.py
+++ django_starter/models.py
@@ -2,4 +2,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    @property
+    def display_name(self):
+        return self.first_name or self.username
