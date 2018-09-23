fe9e93cad405adaced6d3aa732509b7ed5140d5e
Dan Tao <daniel.tao@gmail.com>
2018-09-23 19:52:36 -0400

    Refactor forms to use a shared template for the whole form

--- django_starter/templatetags/form_extras.py
+++ django_starter/templatetags/form_extras.py
@@ -3,11 +3,9 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('registration/_field.html')
-def field(form, field_name):
+@register.inclusion_tag('registration/_form.html')
+def form_tag(form_id, form):
     return {
-        'form': form,
-        'field': form[field_name],
-        'widget': form.fields[field_name].widget,
-        'value': form[field_name].value() or ''
+        'form_id': form_id,
+        'form': form
     }
