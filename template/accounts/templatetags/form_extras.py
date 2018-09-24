ca8b9a8afc3c79df29747f440af3c4b67a8577c1
Dan Tao <daniel.tao@gmail.com>
2018-09-23 21:42:07 -0400

    Make form submit label configurable
    This allows the login and registration forms to have different labels

--- template/accounts/templatetags/form_extras.py
+++ template/accounts/templatetags/form_extras.py
@@ -4,8 +4,9 @@ register = template.Library()
 
 
 @register.inclusion_tag('registration/_form.html')
-def form_tag(form_id, form):
+def form_tag(form, form_id, submit_label):
     return {
+        'form': form,
         'form_id': form_id,
-        'form': form
+        'submit_label': submit_label
     }
