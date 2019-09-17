43b4206742dfb53754b720e53c01d01560bf0a9d
Dan Tao <daniel.tao@gmail.com>
2019-09-17 05:50:49 -0500

    Move form-related template and template tags to project app

---
+++ template/project/templatetags/form_extras.py
@@ -0,0 +1,34 @@
+"""Template tags to assist in rendering forms."""
+
+from django import forms
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('_form.html')
+def form_tag(form, form_id, submit_label):
+    """Render a <form> with the appropriate Bootstrap structure."""
+    return {
+        'form': form,
+        'form_id': form_id,
+        'submit_label': submit_label
+    }
+
+
+@register.filter
+def is_textarea(field):
+    """Indicate whether a field should be rendered as a <textarea>."""
+    return isinstance(field.field.widget, forms.Textarea)
+
+
+@register.filter
+def field_label(field):
+    """Provide a suitable field label."""
+    return field.help_text or field.label
+
+
+@register.filter
+def field_value(field):
+    """Provide a suitable field value defaulting to a blank string."""
+    return field.value() or ''
